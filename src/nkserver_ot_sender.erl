7fa2e393ed1585b12c17672e518defea78a0560c
Carlos González <carlos@erlnets.com>
2020-02-07 20:44:03 +0100

    Lower log

--- src/nkserver_ot_sender.erl
+++ src/nkserver_ot_sender.erl
@@ -181,9 +181,9 @@ do_send_spans(Url, Spans, Total) ->
             {message_queue_len, QueueLen} = process_info(self(), message_queue_len),
             case Total >= 1000 of
                 true ->
-                    lager:info("Sent ~p/~p SPANs (waiting ~p)", [1000, Total, QueueLen]);
+                    lager:debug("Sent ~p/~p SPANs (waiting ~p)", [1000, Total, QueueLen]);
                 false ->
-                    lager:info("Sent ~p SPANs (waiting ~p)", [Total, QueueLen])
+                    lager:debug("Sent ~p SPANs (waiting ~p)", [Total, QueueLen])
             end,
             nkserver_ot_snapshots:snapshot(
                 [?MODULE, send_buffer, ok],
